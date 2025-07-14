86e185d385d0c794e1bece3130d498e2b6232db4
spywar18 <singh.shikhar0712@gmail.com>
2025-07-15 03:38:59 +0530

    starting backend

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+const app = express();
+await connectDB();
+
+//Midlewares
+app.use(cors())
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res)=> res.send('API is working'))
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port', PORT);
+})
+
+
