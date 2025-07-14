4bd1c39d86b2466f5d65cdfe9cd69fc3782bdbf4
spywar18 <singh.shikhar0712@gmail.com>
2025-07-15 03:58:07 +0530

    Updated server.js with new API route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './config/db.js';
 
+
 const app = express();
 await connectDB();
 
@@ -16,7 +17,7 @@ app.get('/', (req, res)=> res.send('API is working'))
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    console.log('Server is running on port', PORT);
+    console.log('Server is running on port', PORT)
 })
 
 
