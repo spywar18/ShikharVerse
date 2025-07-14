c80ab1893f57b6e37bab4b4377ff3bf59363972e
spywar18 <singh.shikhar0712@gmail.com>
2025-07-14 20:35:55 +0530

    client completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
+import Layout from './pages/admin/Layout';
+import Dashboard from './pages/admin/Dashboard';
+import Addblog from './pages/admin/AddBlog';
+import ListBlog from './pages/admin/ListBlog';
+import Comments from './pages/admin/Comments';
+import Login  from './components/admin/Login';
+import 'quill/dist/quill.snow.css';
 
 
 const App = () => {
@@ -10,6 +17,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path='addBlog' element={<Addblog />} />
+          <Route path='listBlog' element={<ListBlog />} />
+          <Route path='comments' element={<Comments />} />
+        </Route>
       </Routes>  
     </div>
   )
