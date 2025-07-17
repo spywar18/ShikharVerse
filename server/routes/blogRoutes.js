6e4ce5a5637b47bfa51ccc7ae99d776683d2095f
spywar18 <singh.shikhar0712@gmail.com>
2025-07-18 02:36:51 +0530

    Creating API endpoints

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import { addBlog } from '../controllers/blogController.js';
+import { addBlog, addComment, deleteBlogById, getAllBlogs, getBlogById, getBlogComments, togglePublish } from "../controllers/blogController.js";
 import upload from '../middleware/multer.js';
 import auth from '../middleware/auth.js';
 
 const blogRouter = express.Router();
 
 blogRouter.post("/add", upload.single('image'), auth, addBlog);
+blogRouter.get('/all', getAllBlogs);
+blogRouter.get('/:blogId', getBlogById);
+blogRouter.post('/delete', deleteBlogById);
+blogRouter.post('/toggle-publish', auth, togglePublish); 
+blogRouter.post('/add-comment', auth, addComment);
+blogRouter.get('/:blogId/comments', getBlogComments);
+
+
+
 
 export default blogRouter;
