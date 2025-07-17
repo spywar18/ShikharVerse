0ec3de8d5058d1b0cdece0bc235c56e6026b5be6
spywar18 <singh.shikhar0712@gmail.com>
2025-07-17 23:25:05 +0530

    setting up routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
 
 
 
@@ -13,6 +14,9 @@ await connectDB();
 app.use(cors())
 app.use(express.json());
 app.use('/api/admin', adminRouter);
+app.use('/api/blog', blogRouter);
+
+
 
 //Routes
 app.get('/', (req, res)=> res.send('API is working'))
